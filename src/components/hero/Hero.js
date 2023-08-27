e50ae80446b399c06504df8b53215d4af0aecb7b
Shihui Huang <sh4480@nyu.edu>
2023-08-27 11:07:41 -0400

    review function

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,16 +4,23 @@ import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import { Button } from 'react-bootstrap';
+import reviews from '../reviews/Reviews';
 
 const Hero = ({movies}) => {
+
+    const navigate = useNavigate();
+    function reviews(movieId){
+        navigate(`/Reviews/$(movieId)`);
+    }
   return (
     <div className = 'movie-carousel-container'>
         <Carousel>
             {
                 movies?.map((movie)=>{
                     return(
-                        <Paper>
+                        <Paper kye={movie.imdbId}>
                             <div className = 'movie-card-container'>
                                 <div className = 'movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
                                     <div className = 'movie-detail'>
@@ -31,6 +38,10 @@ const Hero = ({movies}) => {
                                                 />
                                             </div>
                                             </Link>
+                                            
+                                            <div className="movie-review-button-container">
+                                                <Button variant = "info" onClick ={()=>reviews(movie.imdbId)} > Review </Button>
+                                            </div>
                                         </div>
 
                                     </div>
